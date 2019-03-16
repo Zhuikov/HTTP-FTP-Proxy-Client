641530cd85efed0c9971e77b3499221e225cf4b2
Zhuikov <artem.zhuikov@mail.ru>
2019-03-16 23:22:11 +0300

    centering buttons

--- src/main/java/HttpFtpProxyClient/HttpFtpProxyClient.java
+++ src/main/java/HttpFtpProxyClient/HttpFtpProxyClient.java
@@ -1,40 +1,78 @@
 package HttpFtpProxyClient;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class HttpFtpProxyClient {
 
     public static void main(String[] args) {
-        new HttpFtpProxyClient();
+        new HttpFtpProxyClient(2);
     }
 
-    public HttpFtpProxyClient() {
+//    public HttpFtpProxyClient() {
+//        SwingUtilities.invokeLater(() -> {
+//            JFrame frame = new JFrame("Test");
+//            frame.add(new MenuPane());
+//            frame.setSize(800, 480);
+//            frame.setResizable(false);
+////            frame.pack();
+//            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//            frame.setLocationRelativeTo(null);
+//            frame.setVisible(true);
+//        });
+//    }
+//
+//    class MenuPane extends JPanel {
+//        public MenuPane() {
+//            setBorder(new EmptyBorder(10, 10, 10, 10));
+//            setLayout(new GridBagLayout());
+//
+//            GridBagConstraints gbc = new GridBagConstraints();
+//            gbc.gridwidth = GridBagConstraints.REMAINDER;
+////            gbc.anchor = GridBagConstraints.NORTH;
+////
+////            add(new JLabel("<html><h1><strong><i>Krisko Beatz Quiz</i></strong></h1><hr></html>"), gbc);
+//
+//            gbc.anchor = GridBagConstraints.CENTER;
+//            gbc.fill = GridBagConstraints.HORIZONTAL;
+//
+//            JPanel buttons = new JPanel(new GridBagLayout());
+//            buttons.add(new JButton("Start"), gbc);
+//            buttons.add(new JButton("Show scores"), gbc);
+//            buttons.add(new JButton("Help"), gbc);
+//            buttons.add(new JButton("Exit"), gbc);
+//
+//            gbc.weighty = 1;
+//            add(buttons, gbc);
+//        }
+//    }
+
+    public HttpFtpProxyClient(int q) {
         JFrame frame = new JFrame("Hello");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel labelPane = new JPanel(new GridBagLayout());
 
         labelPane.setLayout(new GridBagLayout());
-        labelPane.setPreferredSize(new Dimension(200, 200));
-        labelPane.setMaximumSize(new Dimension(200 , 200));
 
         GridBagConstraints c = new GridBagConstraints();
         c.anchor = GridBagConstraints.CENTER;
-//        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.gridwidth = GridBagConstraints.REMAINDER;
         c.fill = GridBagConstraints.VERTICAL;
 
         JLabel label1 = new JLabel("Label1");
-//        label1.setSize(20, 100);
-//        label1.setPreferredSize(new Dimension(20, 100));
         JLabel label2 = new JLabel("Label2");
-//        label2.setSize(20, 100);
         JLabel label3 = new JLabel("Label3");
-//        label3.setSize(20, 100);
+
+        JButton but1 = new JButton("Test");
+//        but1.setSize(150, 30);
+        but1.setPreferredSize(new Dimension(150, 30));
+        JButton but2 = new JButton("Vere long test button with text");
 
         labelPane.add(label1, c);
-        labelPane.add(label2, c);
-        labelPane.add(label3, c);
+        labelPane.add(but1, c);
+        labelPane.add(but2, c);
 
         labelPane.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createTitledBorder("Test border title"),
